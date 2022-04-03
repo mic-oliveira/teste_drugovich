b6e70c9d710eeb867e8757724af82bce36e2a007
Michael de Oliveira Ferreira <oliveiramichael@outlook.com>
2022-04-03 02:17:36 -0300

    testes sistema

--- tests/Unit/AddCustomerToGroupTest.php
+++ tests/Unit/AddCustomerToGroupTest.php
@@ -16,6 +16,6 @@ test('deve adicionar cliente em um grupo', function (int $customer_id, int $grou
     Group::factory()->count(2)->create();
     $result = AddCustomerToGroup::run($customer_id, $group_id);
     expect($result->id)->toBe($customer_id);
-    expect($result->group_id)->toBe($group_id);
+    expect((int)$result->group_id)->toBe($group_id);
 })->with('group dataset');
 
